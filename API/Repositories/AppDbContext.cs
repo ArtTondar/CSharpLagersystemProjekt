116d6e9928e31420177e01b72bed49e3f2fd2327
AzureAD\GeorgeVæra <george.vaera@specialisterne.dk>
2026-03-09 14:58:33 +0100

    Add models Add DBConn Add EF

---
+++ API/Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
