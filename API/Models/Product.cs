5c39ec4f21870380a267d9c88112ebd72cabb107
AzureAD\GeorgeVæra <george.vaera@specialisterne.dk>
2026-03-11 19:47:23 +0100

    Add migrations Change models so that you can navigate from parent objects to children (order ->orderlines) even though the children have relational mapping through foreign keys. Fix default connection so .GetConnectionStrings can access it. Add money precision to decimal values in the database. Add Migration steps ReadMe.

--- API/Models/Product.cs
+++ API/Models/Product.cs
@@ -7,7 +7,7 @@
         public string Description { get; set; }
         public decimal UnitPrice { get; set; }
         public int Size { get; set; }
-        public string Warehouse {  get; set; }
+        public string Warehouse { get; set; } = "default";
         public int UnitStock { get; set; }
         public UnitStatus UnitStatus { get; set; }
     }
