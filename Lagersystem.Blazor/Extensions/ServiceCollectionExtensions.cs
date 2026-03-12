d196d6e448feac279c8bce5b324673392a66bfdf
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-12 12:15:32 +0100

    feat: add dependency injection for application services

--- Lagersystem.Blazor/Extensions/ServiceCollectionExtensions.cs
+++ Lagersystem.Blazor/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace Lagersystem.Blazor.Extensions
+﻿using Lagersystem.Blazor.Services.Abstractions;
+using Lagersystem.Blazor.Services.Api;
+
+namespace Lagersystem.Blazor.Extensions;
+
+public static class ServiceCollectionExtensions
 {
-    public class ServiceCollectionExtensions
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddScoped<IProductService, ProductApiService>();
+        services.AddScoped<IOrderService, OrderApiService>();
+        services.AddScoped<ICustomerService, CustomerApiService>();
+
+        return services;
     }
 }
