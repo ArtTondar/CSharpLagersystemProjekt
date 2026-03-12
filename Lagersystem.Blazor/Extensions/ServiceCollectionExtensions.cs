ce7f7b52aca45a0c0f6823a0535cf37e01192f1e
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-12 12:20:06 +0100

    feat: add product state service

--- Lagersystem.Blazor/Extensions/ServiceCollectionExtensions.cs
+++ Lagersystem.Blazor/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Lagersystem.Blazor.Services.Abstractions;
 using Lagersystem.Blazor.Services.Api;
+using Lagersystem.Blazor.State;
 
 namespace Lagersystem.Blazor.Extensions;
 
@@ -10,7 +11,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IProductService, ProductApiService>();
         services.AddScoped<IOrderService, OrderApiService>();
         services.AddScoped<ICustomerService, CustomerApiService>();
-
+        services.AddScoped<ProductState>();
         return services;
     }
 }
