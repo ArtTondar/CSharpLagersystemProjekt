431b49c6d4fde0091186d376d3d097123e20edbd
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-12 10:35:17 +0100

    feat: add DTO models for product, order, customer and user

--- Lagersystem.Blazor/Models/Dtos/ProductDto.cs
+++ Lagersystem.Blazor/Models/Dtos/ProductDto.cs
@@ -1,6 +1,20 @@
-﻿namespace Lagersystem.Blazor.Models.DTOs
+﻿namespace Lagersystem.Blazor.Models.Dtos;
+
+public class ProductDto
 {
-    public class ProductDto
-    {
-    }
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal UnitPrice { get; set; }
+
+    public int Size { get; set; }
+
+    public string Warehouse { get; set; } = string.Empty;
+
+    public int UnitStock { get; set; }
+
+    public UnitStatus UnitStatus { get; set; }
 }
