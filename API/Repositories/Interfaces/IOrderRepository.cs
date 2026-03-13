289e211fe53c0d611f2f34877e8788a41ded1ca3
AzureAD\GeorgeVæra <george.vaera@specialisterne.dk>
2026-03-13 11:12:37 +0100

    Add Order Repository

---
+++ API/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Models;
+
+namespace API.Repositories.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<Order?> GetById(Guid id);
+
+        public Task<List<Order>> GetAll();
+
+        public Task<List<Order>> GetByDate(DateTime start, DateTime? end=null);
+
+        public Task<List<Order>> GetByTotalPrice(decimal totalPrice);
+
+        public Task<Order> Create(Order order);
+
+        public Task Update(Order order);
+
+        public Task Delete(Order order);
+    }
+}
