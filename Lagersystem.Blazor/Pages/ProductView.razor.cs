c871c86e1cbc7c5d7edf9113b7889f23fa632a97
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-13 10:48:17 +0100

    feat: add simple view search logic

---
+++ Lagersystem.Blazor/Pages/ProductView.razor.cs
@@ -0,0 +1,25 @@
+﻿using Lagersystem.Blazor.State;
+using Microsoft.AspNetCore.Components;
+
+namespace Lagersystem.Blazor.Pages;
+
+public partial class ProductView
+{
+    [Inject]
+    public ProductState ProductState { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await ProductState.LoadProductsAsync();
+    }
+
+    private void OnSearchChanged(string value)
+    {
+        ProductState.SearchTerm = value;
+    }
+
+    private void ClearFilters()
+    {
+        ProductState.ClearAllFilters();
+    }
+}
