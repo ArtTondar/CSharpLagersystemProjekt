ded3434a0df0126f2a0d79c92bdf22c4051e7574
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-13 11:24:19 +0100

    feat: extend ProductView.razor.cs functions for better sorting later on

--- Lagersystem.Blazor/Pages/ProductView.razor.cs
+++ Lagersystem.Blazor/Pages/ProductView.razor.cs
@@ -1,4 +1,5 @@
-﻿using Lagersystem.Blazor.State;
+﻿using Lagersystem.Blazor.Models.Dtos;
+using Lagersystem.Blazor.State;
 using Microsoft.AspNetCore.Components;
 
 namespace Lagersystem.Blazor.Pages;
@@ -8,18 +9,63 @@ public partial class ProductView
     [Inject]
     public ProductState ProductState { get; set; } = default!;
 
+    public IReadOnlyList<ProductDto> Products => ProductState.FilteredProducts;
+
+    public bool IsLoading => ProductState.IsLoading;
+
+    public string SearchTerm
+    {
+        get => ProductState.SearchTerm;
+        set => ProductState.SearchTerm = value;
+    }
+
+    public int? SelectedSize
+    {
+        get => ProductState.SelectedSize;
+        set => ProductState.SelectedSize = value;
+    }
+
+    public string SelectedWarehouse
+    {
+        get => ProductState.SelectedWarehouse;
+        set => ProductState.SelectedWarehouse = value;
+    }
+
+    public UnitStatus? SelectedUnitStatus
+    {
+        get => ProductState.SelectedUnitStatus;
+        set => ProductState.SelectedUnitStatus = value;
+    }
+
+    public string SortField
+    {
+        get => ProductState.SortField;
+        set => ProductState.SortField = value;
+    }
+
+    public bool SortDescending
+    {
+        get => ProductState.SortDescending;
+        set => ProductState.SortDescending = value;
+    }
+
     protected override async Task OnInitializedAsync()
     {
         await ProductState.LoadProductsAsync();
     }
 
-    private void OnSearchChanged(string value)
-    {
-        ProductState.SearchTerm = value;
-    }
-
     private void ClearFilters()
     {
         ProductState.ClearAllFilters();
+    }
+
+    private void SelectProduct(ProductDto product)
+    {
+        ProductState.SelectProduct(product);
+    }
+
+    private void ToggleSortDirection()
+    {
+        SortDescending = !SortDescending;
     }
 }
