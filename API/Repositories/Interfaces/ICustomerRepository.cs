8b6e12c92e7d6affcf11104d3fe6137f0497d0a1
AzureAD\GeorgeVæra <george.vaera@specialisterne.dk>
2026-03-13 11:23:02 +0100

    Add customer repository

---
+++ API/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+
+namespace API.Repositories.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        public Task<Customer?> GetById(Guid id);
+        public Task<Customer?> GetByEmail(string email);
+        public Task<Customer?> GetByPhone(string phone);
+        public Task<List<Customer>> GetAll();
+        public Task<Customer> Create(Customer customer);
+        public Task Update(Customer customer);
+        public Task Delete(Customer customer);
+    }
+}
