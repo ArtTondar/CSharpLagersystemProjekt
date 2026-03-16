b1d6ea8873fb71383b70e8d1518b1e487082e89e
Jannie Brunhøj <72923791+ArtTondar@users.noreply.github.com>
2026-03-16 10:50:20 +0100

    Added OrderController and GetByCustomerId() method in

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,6 +1,5 @@
 ﻿using API.Models;
-using API.Repositories.DbAccess;
-using API.Repositories.DbAccess.Abstractions;
+using API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,23 +8,32 @@ namespace API.Controllers
     [ApiController]
     public class OrderController : Controller
     {
-        private readonly IOrderAccess _orderAccess;
-        public OrderController(OrderAccess orderAccess)
+        private readonly IOrderRepository _repo;
+        public OrderController(IOrderRepository repo)
         {
-            _orderAccess = orderAccess;
+            _repo = repo;
+        }
+        private IActionResult OkOrNotFound<T>(List<T> list)
+        {
+            return (list == null || !list.Any()) ? NotFound() : Ok(list);
         }
 
         [HttpGet("get-order-by-id/{id}")]
         public async Task<IActionResult> GetOrderById(Guid id)
         {
-            Order order = await _orderAccess.GetOrderById(id);
-
-            if (order != null)
+            try
             {
+                Order? order = await _repo.GetById(id);
+                if (order == null)
+                {
+                    return NotFound();
+                }
                 return Ok(order);
             }
-
-            return StatusCode(500, "An error occurred while retrieving order.");
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occured while retrieving order.");
+            }
         }
 
         [HttpGet]
@@ -33,14 +41,11 @@ namespace API.Controllers
         {
             try
             {
-                var orders = await _orderAccess.GetOrders();
-                if (orders == null || !orders.Any())
-                {
-                    return NotFound();
-                }
-                return Ok(orders);
+                List<Order> orders = await _repo.GetAll();
+
+                return OkOrNotFound(orders);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return StatusCode(500, "An error occurred while retrieving orders.");
             }
@@ -51,39 +56,110 @@ namespace API.Controllers
         {
             try
             {
-                var orders = await _orderAccess.GetOrdersByCustomerId(customerId);
-                if (orders == null || !orders.Any())
-                {
-                    return NotFound();
-                }
-                return Ok(orders);
+                List<Order> orders = await _repo.GetByCustomerId(customerId);
+                return OkOrNotFound(orders);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return StatusCode(500, "An error occurred while retrieving orders.");
             }
         }
 
-        [HttpGet("get-orders-by-date-range")]
+        [HttpGet("get-orders-by-date")]
         public async Task<IActionResult> GetOrdersByDateRange([FromQuery] DateTime startDate, DateTime endDate)
         {
             try
             {
-                var orders = await _orderAccess.GetOrdersByDateRange(startDate, endDate);
-                if (orders == null || !orders.Any())
-                {
-                    return NotFound();
-                }
-                return Ok(orders);
+                List<Order> orders = await _repo.GetByDate(startDate, endDate);
+                return OkOrNotFound(orders);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return StatusCode(500, "An error occurred while retrieving orders.");
             }
         }
 
-
-
+        [HttpGet("get-orders-by-total-price/{totalPrice}")]
+        public async Task<IActionResult> GetOrdersByTotalPrice(decimal totalPrice)
+        {
+            try
+            {
+                List<Order> orders = await _repo.GetByTotalPrice(totalPrice);
+                return OkOrNotFound(orders);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while retrieving orders.");
+            }
 
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Order createdOrder = await _repo.Create(order);
+                return CreatedAtAction(nameof(GetOrderById), new { id = createdOrder.Id}, createdOrder);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while creating order.");
+            }
+        }
+
+        [HttpPut("/{id}")]
+        public async Task<IActionResult> UpdateOrder(Guid id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingOrder = await _repo.GetById(id);
+            if (existingOrder == null)
+            {
+                return NotFound();
+            } 
+
+            try
+            {
+                await _repo.Update(order);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while updating order.");
+            }
+        }
+
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> DeleteOrder(Guid id)
+        {
+            Order? existingOrder = await _repo.GetById(id);
+            if (existingOrder == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _repo.Delete(existingOrder);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while deleting order.");
+            }
+        }
+    }
 }
