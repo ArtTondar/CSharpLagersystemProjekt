379b5c5279c88a1e1700078874a1476c84b55e67
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-03-16 14:16:54 +0100

    feat: Add Update Order Logic

--- Lagersystem.Blazor/Models/Requests/UpdateOrderRequest.cs
+++ Lagersystem.Blazor/Models/Requests/UpdateOrderRequest.cs
@@ -2,7 +2,12 @@
 
 public class UpdateOrderRequest
 {
+    // Id skal være med i body for at matche API'et.
+    public Guid Id { get; set; }
+
     public Guid CustomerId { get; set; }
+
+    public DateTime OrderDate { get; set; }
 
     public decimal TotalPrice { get; set; }
 }
